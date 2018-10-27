72d2b3ae5db8551eba62ff8e48de316f4b57de6d
Eros <george.marr.cobc@gmail.com>
2018-10-27 11:10:07 +0100

    cba to fix errors rn

--- HeartbeatTesting/HeartbeatTesting/mongo.c
+++ HeartbeatTesting/HeartbeatTesting/mongo.c
@@ -1,9 +1,9 @@
 #include "../packages/mongo/src/libmongoc/src/mongoc/mongoc.h"
 
-#define MONGO_URI "mongodb://localhost:27017"
 
 int handle_mongo(int argc, const char argv[]) {
 
+	const char *mongo_uri "mongodb://localhost:27017"
 	mongo_uri_t *uri;
 	mongo_client_t *client;
 	mongo_database_t *database;
@@ -16,6 +16,12 @@ int handle_mongo(int argc, const char argv[]) {
 	mongo_init();
 
 	if (argc > 1) {
+		mongo_uri = argv[1];
+	}
+
+	uri = mongo_uri_new_with_error(mongo_uri, &error);
+
+	if (!uri) {
 
 	}
 }
