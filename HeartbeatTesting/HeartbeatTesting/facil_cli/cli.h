d67db92bfb86632eaea95c895f4dbe32e001eb52
George <george.marr.cobc@gmail.com>
2019-04-11 19:44:53 +0100

    remove a ton of the old stuff

--- HeartbeatTesting/HeartbeatTesting/facil_cli/cli.h
+++
@@ -1,9 +0,0 @@
-#ifndef APP_CLI_H
-
-#define APP_CLI_H
-
-void init_cli(int argc, const char *argv[]);
-void free_cli(void);
-void init_http_service(void);
-
-#endif
--- HeartbeatTesting/HeartbeatTesting/facil_cli/main_cli.h
+++
@@ -1,7 +0,0 @@
-
-//
-// Created by George Marr on 25/10/2018.
-//
-
-#include "../../packages/facil.io/lib/facil/fio.h"
-#include "cli.h"
