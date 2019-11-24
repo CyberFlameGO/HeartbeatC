663bdf46c22d07a433a1262728960edfa78387bc
George <george.marr.cobc@gmail.com>
2019-11-24 15:45:59 +0000

    make it run every 30 seconds

--- Heartbeat.c
+++ Heartbeat.c
@@ -1,5 +1,15 @@
-
+#include "monitor.h"
 
 int main(void) {
+
+    fprintf(stdout, "----- [HEARTBEAT] -----");
+
+    while (true) {
+        display_battery_life();
+        display_cpu_load();
+        display_memory_usage();
+        signal(SIGALARM, main);
+        alarm(30);
+    }
     return 0;
 }
